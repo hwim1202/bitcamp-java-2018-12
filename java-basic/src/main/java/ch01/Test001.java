d3c29058e68faaff18390a6482f8c8a454d4153c
hwim1202 <hwim1202@gmail.com>
2019-01-02 18:29:34 +0900

    java-basic 프로젝트 준비

---
+++ java-basic/src/main/java/ch01/Test001.java
@@ -0,0 +1,6 @@
+package ch01;
+public class Test001 {
+    public static void main(String[] args) {
+    System.out.println("Hello!");
+    }
+}
---
+++ java-basic/src/main/java/ch01/Test03.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class Test03 {
+
+    @Override
+    public String toString() {
+        return "ok";
+    }
+    
+    public static void main(String[] args) {
+    System.out.println("Hello!");
+    }
+}
