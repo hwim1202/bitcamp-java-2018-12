1db147c3ecd104a46a206a50d4ac1b0c7f139faf
hwim1202 <hwim1202@gmail.com>
2019-01-03 18:22:43 +0900

    .

--- java-project/src/main/java/bitcamp/lms/App.java
+++ java-project/src/main/java/bitcamp/lms/App.java
@@ -5,7 +5,13 @@ package bitcamp.lms;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "번호: 1\n" + 
+            "수업명: 자바 프로젝트 실습\n" + 
+            "설명: 자바 프로젝트를 통한 자바 언어 활용법 익히기\n" + 
+            "시작일: 2019-01-02\n" + 
+            "종료일: 2019-05-28\n" + 
+            "총수업시간: 1000 시간\n" + 
+            "일수업시간: 8 시간";
     }
 
     public static void main(String[] args) {
