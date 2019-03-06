99dac7888e35705a892d5dffa4326f9d823a3629
hyunwoo <hwim1202@gmail.com>
2019-03-06 17:50:45 +0900

    .

---
+++ java-basic/src/main/java/ch26/d/Test03.java
@@ -0,0 +1,25 @@
+package ch26.d;
+
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Test03 {
+
+  public static void main(String[] args) throws Exception {
+    
+    InputStream inputStream = Resources.getResourceAsStream(
+        "ch26/d/mybatis-config.xml");
+    SqlSessionFactory sqlSessionFactory =
+      new SqlSessionFactoryBuilder().build(inputStream);
+
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    
+    Board2 board = sqlSession.selectOne("board.select4");
+    System.out.println(board);
+    
+  }
+
+}
